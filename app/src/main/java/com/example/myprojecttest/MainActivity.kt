36020c17a6430a4bef977c252f0f8c18fc550969
farshid <sputnick01.farshid75@gmail.com>
2023-11-25 22:30:04 +0330

    add Button

--- app/src/main/java/com/example/myprojecttest/MainActivity.kt
+++ app/src/main/java/com/example/myprojecttest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myprojecttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn:Button
+    lateinit var btn2: Button
+    lateinit var btn3:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
